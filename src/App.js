ae1e7d226997adc90ab29875ea30b1c79cab49eb
Dev Jobalia <webdev.jobalia@gmail.com>
2023-04-05 13:08:28 +0530

    🦾Aminmation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        layout
+        className="card"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          borderRadius: "1rem",
+          boxShadow: "0px 10px 30px rgba(0,0,0,0.5)",
+        }}
+      >
+        <motion.h2 layout="position" h2>
+          Framer Motion 🚀
+        </motion.h2>
+        {isOpen && (
+          <motion.div
+            className="expand"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Doloremque vitae distinctio illo soluta, voluptatum tempore
+              facilis expedita aperiam sed reiciendis?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+              consequuntur?
+            </p>
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
